21fd9db5325a2dd8b47895422f4ce25edafe0916
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-11-17 01:32:03 +0200

    fixed deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,9 @@ import { build } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false
-  ,
-  nitro: {
-    preset: 'node-server'
+  target: 'static',
+  router: {
+    base: '/Wallet/',
   },
  
   // ... other options
