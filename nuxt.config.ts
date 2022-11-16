513a8b7dc653027be06a9e8b67f13edc49f6a68a
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-11-17 00:44:40 +0200

    chenged config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import { build } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false
+  ,
   nitro: {
     preset: 'node-server'
   },
